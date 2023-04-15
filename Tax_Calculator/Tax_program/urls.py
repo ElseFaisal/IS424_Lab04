1524e0bd31f0858a7a0af5c3ac6690a26399b5f9
ElseFaisal <94870351+ElseFaisal@users.noreply.github.com>
2023-04-16 02:42:42 +0300

    Add the second view called calculate_TotalPrice

--- Tax_Calculator/Tax_program/urls.py
+++ Tax_Calculator/Tax_program/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.default,name="Defalut site")
+    path("",views.default,name="Defalut site"),
+    path("<float:price>",views.calculate_TotalPrice,name="Calculate total price")
 ]
--- Tax_Calculator/Tax_program/views.py
+++ Tax_Calculator/Tax_program/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+TAX_RATE = 0.15
+
 def default(request):
     return render(request,'Tax_program/default.html')
+
+def calculate_TotalPrice(request,price):
+    return render(request,'Tax_program/calcTotPrice.html',{"price":(price*(1+TAX_RATE))})
